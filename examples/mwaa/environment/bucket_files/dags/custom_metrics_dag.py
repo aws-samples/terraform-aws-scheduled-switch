2b8ca07a080a4ead4e0270e1c1e14a791a721118
Farhan Angullia <angullia@amazon.com>
2022-07-06 11:09:54 +0800

    add source url

--- examples/mwaa/environment/bucket_files/dags/custom_metrics_dag.py
+++ examples/mwaa/environment/bucket_files/dags/custom_metrics_dag.py
@@ -1,4 +1,5 @@
 # Copyright © 2022 Amazon Web Services, Inc. or its affiliates. All Rights Reserved. This AWS Content is provided subject to the terms of the AWS Customer Agreement available at http://aws.amazon.com/agreement or other written agreement between Customer and either Amazon Web Services, Inc. or Amazon Web Services EMEA SARL or both."
+# Source: https://docs.aws.amazon.com/mwaa/latest/userguide/samples-custom-metrics.html
 
 import os
 import socket
